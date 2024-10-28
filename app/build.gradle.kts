f0d38f4bdb4daf0b2e99440b170d8aae70e8cd9f
Finn Welsford-Ackroyd <finn@geneious.com>
2024-10-29 00:26:46 +1300

    Use native-image gradle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.graalvm.buildtools.gradle.tasks.BuildNativeImageTask
+
 plugins {
     application
+    // https://github.com/GradleUp/shadow/releases
     id("com.gradleup.shadow") version "8.3.3"
+    // https://graalvm.github.io/native-build-tools/latest/index.html#changelog
+    id("org.graalvm.buildtools.native") version "0.10.3"
 }
 
 repositories {
@@ -24,4 +30,36 @@ application {
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+// https://graalvm.github.io/native-build-tools/latest/gradle-plugin.html
+graalvmNative.binaries.all {
+    imageName.set("tahia")
+    useFatJar.set(true)
+    debug.set(false)
+    verbose.set(false)
+
+    val iprofFile = file("./graal/default.iprof")
+    val logFile = file("./build/native/nativeCompile/build.log")
+    buildArgs.addAll(
+        // https://www.graalvm.org/latest/reference-manual/native-image/optimizations-and-performance/#optimization-levels
+        "-Ob",
+        "--gc=G1",
+        // "--pgo-instrument",
+        "--pgo=$iprofFile",
+        "--exact-reachability-metadata",
+        // "--initialize-at-run-time=org.eclipse.jdt.internal.compiler.util.Messages",
+        "--emit build-report",
+        "-Dgraal.LogFile=$logFile",
+    )
+}
+
+// Make GraalVM use shadowJar as input
+tasks.named<BuildNativeImageTask>("nativeCompile") {
+    classpathJar.set(tasks.named<ShadowJar>("shadowJar").flatMap { it.archiveFile })
+}
+
+task<Exec>("benchmark") {
+    // https://github.com/sharkdp/hyperfine
+    commandLine("hyperfine")
 }
